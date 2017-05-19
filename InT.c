233132ade264d30d64ee9ff25c18603d93aad54d
arkainoh <arkainoh@gmail.com>
2017-05-19 16:42:21 +0900

    add feed forwarding calculations

--- InT.c
+++ InT.c
@@ -1,17 +1,26 @@
 #include <stdio.h>
 #include <string.h>
 #define INPUT_LENGTH 784
-#define HL_SIZE 512
+#define HL_LENGTH 512
 #define OUTPUT_LENGTH 10
 #define TOKEN_LENGTH 25 // pos num: 24, neg num: 25
 #define BUFFER_SIZE 1024
 
-double X[INPUT_LENGTH + 1];
-double W1[INPUT_LENGTH][HL_SIZE];
-double W2[HL_SIZE][HL_SIZE];
-double W3[HL_SIZE][HL_SIZE];
-double W4[HL_SIZE][HL_SIZE];
-double W5[HL_SIZE][OUTPUT_LENGTH];
+long double X[INPUT_LENGTH + 1];
+
+long double W1[INPUT_LENGTH][HL_LENGTH];
+long double W2[HL_LENGTH][HL_LENGTH];
+long double W3[HL_LENGTH][HL_LENGTH];
+long double W4[HL_LENGTH][HL_LENGTH];
+long double W5[HL_LENGTH][OUTPUT_LENGTH];
+
+long double B1[HL_LENGTH];
+long double B2[HL_LENGTH];
+long double B3[HL_LENGTH];
+long double B4[HL_LENGTH];
+long double B5[OUTPUT_LENGTH];
+
+
 long double test[10][1024];
 /*
 int saveToken(char* token, int idx) {
@@ -24,6 +33,41 @@ int saveToken(char* token, int idx) {
 }
 */
 
+void passIL(int x[INPUT_LENGTH], int w[INPUT_LENGTH][HL_LENGTH], int b[HL_LENGTH], int result[HL_LENGTH]) {
+	
+	for(int n = 0; n < HL_LENGTH; n++) result[n] = b[n]; // copy b to result
+	
+	for(int m = 0; m < INPUT_LENGTH; m++) // row-wise matmul
+		for(int n = 0; n < HL_LENGTH; n++) result[n] += (x[m] * w[m][n]);
+
+	for(int n = 0; n < HL_LENGTH; n++) // reLU
+		if(result[n] < 0) result[n] = 0;
+}
+
+void passHL(int x[HL_LENGTH], int w[HL_LENGTH][HL_LENGTH], int b[HL_LENGTH], int result[HL_LENGTH]) {
+	
+	for(int n = 0; n < HL_LENGTH; n++) result[n] = b[n]; // copy b to result
+	
+	for(int m = 0; m < HL_LENGTH; m++) // row-wise matmul
+		for(int n = 0; n < HL_LENGTH; n++) result[n] += (x[m] * w[m][n]);
+
+	for(int n = 0; n < HL_LENGTH; n++) // reLU
+		if(result[n] < 0) result[n] = 0;
+
+}
+
+void passOL(int x[HL_LENGTH], int w[HL_LENGTH][OUTPUT_LENGTH], int b[OUTPUT_LENGTH], int result[OUTPUT_LENGTH]) {
+	
+	for(int n = 0; n < OUTPUT_LENGTH; n++) result[n] = b[n]; // copy b to result
+	
+	for(int m = 0; m < HL_LENGTH; m++) // row-wise matmul
+		for(int n = 0; n < OUTPUT_LENGTH; n++) result[n] += (x[m] * w[m][n]);
+
+	for(int n = 0; n < OUTPUT_LENGTH; n++) // reLU
+		if(result[n] < 0) result[n] = 0;
+
+}
+
 void loadW(char* filename, long double mat[][]) {
 	char line[BUFFER_SIZE];
 	FILE *f;
@@ -35,14 +79,9 @@ void loadW(char* filename, long double mat[][]) {
 	int rows;
 	int cols;
 	fgets(line, sizeof(line), f);
-	printf("First row: %s\n", line);
 	sscanf(line, "%d %d", &rows, &cols);
 	printf("(%d, %d)\n", rows, cols);
 	while(fgets(line, sizeof(line), f)) {
-		puts("reading line...");
-		puts(line);
-		printf("read length: %d\n", strlen(line));
-		puts("");
 		puts("tokens:");
 		int off = 0;
 
