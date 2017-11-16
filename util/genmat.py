c6c26f4f9075c96bfe163211bf74f1106219276a
arkainoh <arkainoh@gmail.com>
2017-11-16 18:38:05 +0900

    use argv to determine shape

--- util/genmat.py
+++ util/genmat.py
@@ -1,6 +1,7 @@
 import numpy as np
+import sys
 
-m = np.random.rand(3, 5)
+m = np.random.rand(int(sys.argv[1]), int(sys.argv[2]))
 
 f = open('test.w', 'w')
 f.write(str(m.shape[0]) + ' ' + str(m.shape[1]) + '\n')
