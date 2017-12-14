62da0cd38be83610d6312684629e983525d06141
arkainoh <arkainoh@gmail.com>
2017-12-15 01:21:00 +0900

    move MNIST constants into mnist_utils.h

--- mnist/server/src/parameters/parameters.h
+++ mnist/server/src/parameters/parameters.h
@@ -4,8 +4,3 @@
 #define TOKEN_LENGTH 24 // pos num: 24, neg num: 25
 #define BUFFER_SIZE 1024
 
-#define MNIST_MAX_TRAINING_IMAGES 60000 // number of images+labels in the TRAIN file/s
-#define MNIST_MAX_TESTING_IMAGES 10000 // number of images+labels in the TEST file/s
-#define MNIST_IMG_WIDTH 28 // image width in pixel
-#define MNIST_IMG_HEIGHT 28 // image height in pixel
-
